101398785212ab11da177d85c534ada51a9f83ad
eduardonk9999 <thiago.novak7@gmail.com>
2025-05-06 08:26:26 -0300

    start project

---
+++ src/main/java/crud/finances/investimentos/InvestimentosApplication.java
@@ -0,0 +1,13 @@
+package crud.finances.investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentosApplication.class, args);
+	}
+
+}
